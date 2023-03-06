168aaf6dde7c9949cd3e2ba233800e42bda9038b
Elizaveta Gaidarova <gaidarova.liza@gmail.com>
2023-03-06 19:14:06 +0300

    Second commit

---
+++ task18.js
@@ -0,0 +1,90 @@
+// Setup
+let myStr = "Jello World";
+
+// Only change code below this line
+myStr = 'Hello World'; // Change this line
+// Only change code above this line
+
+
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const thirdLetterOfLastName = lastName[2]; // Change this line
+
+// Setup
+const lastNameAgain = "Lovelace";
+
+// Only change code below this line
+const lastLetterOfLastName = lastNameAgain[lastNameAgain.length - 1]; // Change this line
+
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = "myAdjective + myNoun + myVerb + myAdverb"; // Change this line
+// Only change code above this line
+
+
+const myArray = ['hello', 2];
+
+
+// Only change code below this line
+const myArr = [[1, [1, 2, 3]], [string, string]];
+
+
+const myArray2 = [50, 60, 70];
+const myData = meArray[0]
+
+const myArray3 = [
+	[1, 2, 3],
+	[4, 5, 6],
+	[7, 8, 9],
+	[[10, 11, 12], 13, 14],
+];
+
+const myData2 = myArray[2][1];
+
+
+// Setup
+const myArray4 = [["John", 23], ["cat", 2]];
+
+// Only change code below this line
+myArray.push(["dog", 3])
+
+
+// Setup
+const myArray5 = [["John", 23], ["cat", 2]];
+
+// Only change code below this line
+const removedFromMyArray = myArray.pop()
+
+
+const myList = [["Chocolate Bar", 15], ["pizza", 10], ["Soup", 22], ["Bread", 16], ["Sweet", 1], ["Ice-Cream", 20]];
+
+
+
+
+function reusableFunction() {
+	console.log("Hi World");
+}
+
+reusableFunction()
+
+function functionWithArgs(num1, num2) {
+	console.log(num1 + num2)
+}
+
+functionWithArgs(1, 2)
+
+
+// Setup
+
+function processArg(num) {
+	return (num + 3) / 5;
+}
+
+// Only change code below this line
+const processed = processArg(7)
